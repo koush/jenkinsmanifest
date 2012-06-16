9ae8fcf59e624c094fa7c4cbc46be4c6455ba021
Koushik Dutta <koushd@gmail.com>
2012-06-15 18:10:15 -0700

    key by build

--- model.js
+++ model.js
@@ -6,7 +6,7 @@ setting.get('model_version', function(version) {
   if (version == null) {
     mysql.query('create table if not exists settings (name varchar(32) primary key not null, value varchar(256))');
     
-    mysql.query('create table if not exists releases (build int not null, modversion varchar(64) primary key not null, device varchar(32) not null, filename varchar(256) not null, date bigint not null)');
+    mysql.query('create table if not exists releases (build int  primary key not null, modversion varchar(64)not null, device varchar(32) not null, filename varchar(256) not null, date bigint not null)');
 
     version = "1";
   }
