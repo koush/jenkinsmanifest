97e2199fdfa3213bc34aa2e21ab857841e4f077b
Koushik Dutta <koushd@gmail.com>
2012-07-31 19:21:53 -0700

    update link for new get.cm paths

--- app.js
+++ app.js
@@ -294,7 +294,8 @@ function refresh() {
         collections.each(data.artifacts, function(index, artifact) {
           if ((artifact.displayPath.startsWith('cm-') || artifact.displayPath.startsWith('update-')) && artifact.displayPath.endsWith('.zip')) {
             zip = artifact.displayPath;
-            entry.url = 'http://get.cm/get/artifacts/' + build.number + '/artifact/archive/' + artifact.displayPath;
+            // entry.url = 'http://get.cm/get/artifacts/' + build.number + '/artifact/archive/' + artifact.displayPath;
+            entry.url = 'http://get.cm/get/jenkins/' + build.number + '/' + artifact.displayPath;
             return;
           }
           
